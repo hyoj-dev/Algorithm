e2426491726d7c4b41d4992fa99a2e35d5471216
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-08-01 19:31:05 +0900

    [Silver V] Title: 너의 평점은, Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        Map<String, Double> scoreMap = new HashMap<>();
+        double creditSum = 0;
+        double totalScore = 0;
+
+        scoreMap.put("A+", 4.5);
+        scoreMap.put("A0", 4.0);
+        scoreMap.put("B+", 3.5);
+        scoreMap.put("B0", 3.0);
+        scoreMap.put("C+", 2.5);
+        scoreMap.put("C0", 2.0);
+        scoreMap.put("D+", 1.5);
+        scoreMap.put("D0", 1.0);
+        scoreMap.put("F", 0.0);
+
+        for (int i = 0; i < 20; i++) {
+            st = new StringTokenizer(br.readLine());
+            String subject = st.nextToken();
+            double credit = Double.parseDouble(st.nextToken());
+            String grade = st.nextToken();
+
+            if(grade.equals("P")) continue;
+
+            creditSum += credit;
+            totalScore += credit * scoreMap.get(grade);
+        }
+        System.out.println(totalScore / creditSum);
+    }
+}
