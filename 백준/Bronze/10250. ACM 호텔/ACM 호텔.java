6f89e145aff671431f9709eaa9031dd3176757ea
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-09-10 00:52:39 +0900

    [Bronze III] Title: ACM 호텔, Time: 200 ms, Memory: 18196 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -4,14 +4,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = sc.nextInt();
-        int floor = 0;
-        int roomNum = 0;
 
         for (int i = 0; i < T; i++) {
             int h = sc.nextInt();
             int w = sc.nextInt();
             int n = sc.nextInt();
 
+            int floor, roomNum;
+            
             if (n % h == 0) {
                 floor = h;
                 roomNum = n / h;
