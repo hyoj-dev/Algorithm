417e229375f637b3cfa83f4ed4d662e458166cf2
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-10-18 01:32:18 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14284 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -10,19 +10,14 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         String str = br.readLine();
 
-        HashMap<Character, Integer> map = new HashMap<>();
-        for (int i = 0; i < 26; i++) {
-            char key = (char) ('a' + i);
-            int value = 'a' + i - 96;
+        long sum = 0;
+        long r = 1;
+        long M = 1234567891;
 
-            map.put(key, value);
-        }
-
-        int sum = 0;
-        char[] arr = str.toCharArray();
         for (int i = 0; i < N; i++) {
-            char key = arr[i];
-            sum += (int) (map.get(key) * Math.pow(31, i));
+            int value = str.charAt(i) - 'a' + 1;
+            sum = (sum + value * r) % M;
+            r = (r * 31 % M);
         }
 
         System.out.println(sum);
