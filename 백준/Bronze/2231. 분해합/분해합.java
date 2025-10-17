7f48e62a72e68b41c7d2d29f38d8e813100a39c0
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-10-18 01:05:41 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -7,24 +7,24 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String N = br.readLine();
+        int N = Integer.parseInt(br.readLine());
         int answer = 0;
-        int parsed = Integer.parseInt(N);
 
-        while (parsed > 0) {
-            if (deSum(String.valueOf(parsed)) == Integer.parseInt(N)) {
-                answer = parsed;
-                parsed--;
-            } else parsed--;
+        for (int i = 0; i < N; i++) {
+            if (deSum(i) == N) {
+                answer = i;
+                break;
+            }
         }
+
         System.out.println(answer);
     }
 
-    static int deSum(String n) {
-        char[] arr = n.toCharArray();
-        int sum = Integer.parseInt(n);
-        for (char c : arr) {
-            sum += Integer.valueOf(c-48);
+    static int deSum(int n) {
+        int sum = n;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
         }
         return sum;
     }
