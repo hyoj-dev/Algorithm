75318a23f5440ed6358e6c07cf5b49f40ed1c488
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-11-18 23:52:21 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문2, Time: 85 ms, Memory: 24,576 KB -BaekjoonHub

---
+++ SWEA/D3/1229. ［S／W 문제해결 기본］ 8일차 － 암호문2/［S／W 문제해결 기본］ 8일차 － 암호문2.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Solution {
+   public static void main(String[] args) throws IOException {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      StringBuilder sb = new StringBuilder();
+      
+      int T = 10;
+      
+      for (int test_case = 1; test_case <= T; test_case++) {
+         int N = Integer.parseInt(br.readLine());
+         ArrayList<Integer> list = new ArrayList<>();
+         StringTokenizer st = new StringTokenizer(br.readLine());
+         
+         for(int i = 0; i < N ; i++) list.add(Integer.parseInt(st.nextToken()));
+         
+         int M = Integer.parseInt(br.readLine());
+         st = new StringTokenizer(br.readLine());
+
+         int x = 0, y = 0;
+         
+         while(st.hasMoreTokens()) {
+            String command = st.nextToken();
+            switch (command) {
+               case "I":
+                  x = Integer.parseInt(st.nextToken());
+                  y = Integer.parseInt(st.nextToken());
+                  for(int i = 0 ; i < y ; i++) {
+                     int s = Integer.parseInt(st.nextToken());
+                     list.add(x, s);
+                     x++;
+                  }
+                  break;
+               case "D":
+                  x = Integer.parseInt(st.nextToken());
+                  y = Integer.parseInt(st.nextToken());
+                  for(int i = 0; i < y ; i++) list.remove(x);
+                  break;
+            }
+         }
+         
+         sb.append("#").append(test_case).append(" ");
+         for(int i = 0 ; i < 10 ; i++) {
+            sb.append(list.get(i)).append(" ");
+         }
+         sb.append("\n");
+         list.clear();
+      }
+      System.out.print(sb);
+   }
+}
