d79e7366bde57bc574839b01e6516d844b8778d8
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-11-21 16:19:36 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 116 ms, Memory: 27,904 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.java
@@ -1,42 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.format.ResolverStyle;
 import java.util.*;
 
-public class Solution {
+public class Solution {	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int T = Integer.parseInt(br.readLine());
 
-        for (int testCase = 1; testCase <= T; testCase++) {
-            int tc = Integer.parseInt(br.readLine());   //출력 테스트 케이스
+        for (int test_case = 1; test_case <= T; test_case++) {
+        	int N = Integer.parseInt(br.readLine());
+        	HashMap<Integer, Integer> map = new HashMap<>();
+        
+        	StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        	for(int i = 0; i < 1000; i++) {
+        		int key = Integer.parseInt(st.nextToken());
+        		map.put(key, map.getOrDefault(key, 0)+1);
+        	}
+        	
+        	ArrayList<Map.Entry<Integer, Integer>> list = new ArrayList<>(map.entrySet());
+        	 
+        	Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
+				@Override
+				public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2) {
+					int v1 = o1.getValue();
+					int v2 = o2.getValue();
+					if(v2-v1 != 0) return v2-v1;
+					
+					return o2.getKey() - o1.getKey();
+				}
+			});
+			
+        	int answer  = list.get(0).getKey();
 
-            HashMap<Integer, Integer> map = new HashMap<>();
-
-            StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-
-            for (int i = 0; i < 1000; i++) {
-                int num = Integer.parseInt(st1.nextToken());
-
-                map.put(num, map.getOrDefault(num, 0) + 1);
-            }
-
-            int maxV = Integer.MIN_VALUE;
-            int maxK = Integer.MIN_VALUE;
-            for (Map.Entry<Integer, Integer> integerIntegerEntry : map.entrySet()) {
-                int key = integerIntegerEntry.getKey();
-                int value = integerIntegerEntry.getValue();
-
-
-                if (maxV < value) {
-                    maxV = value;
-                    maxK = key;
-                } else if (maxV == value) {
-                    maxK = Math.max(maxK, key);
-                }
-            }
-            System.out.println("#" + tc + " " + maxK);
+        	sb.append("#").append(test_case).append(" ").append(answer).append("\n");
         }
+        System.out.print(sb);
     }
 }
