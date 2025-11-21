aa3d8ad8a13a7219833816b40e4325ae0fbc8dcf
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-11-21 17:44:22 +0900

    [D3] Title: 농작물 수확하기, Time: 96 ms, Memory: 27,136 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -1,41 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.format.ResolverStyle;
 import java.util.*;
 
-public class Solution {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int N = Integer.parseInt(br.readLine());
-			int mid = N/2;
-			int sum = 0;
-			
-			int start = mid, end = mid;
-			
-			
-			for (int i = 0; i < N; i++) {
-				String nums = br.readLine();
-				
-				for (int j = start; j <= end; j++) {
-					sum += nums.charAt(j)-'0';
-				}
-				
-				if(i < mid) { 	//i=0 -> 1 -> 2
-					start--;	//s=2 -> 1 -> 0
-					end++;		//e=2 -> 3 -> 4
-				} 
-				else {			//i=3 -> 4
-					start++;	//s=1 -> 2
-					end--;		//e=3 -> 2
-				}
-			}
-			sb.append("#").append(test_case).append(" ").append(sum).append("\n");
-		}
-		System.out.print(sb);
-	}
+public class Solution {	
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+        	int N = Integer.parseInt(br.readLine());
+        	int[][] arr = new int[N][N];
+        	
+        	for(int i = 0; i < N ;i++) {
+        		String nums = br.readLine();
+        		for(int j = 0 ; j < N ;j++) {
+        			int num = nums.charAt(j) - '0';
+        			arr[i][j] = num;
+        		}
+        	}
+        	
+        	int mid = N / 2;
+        	int answer = 0;
+        	for(int i = 0 ; i < N ;i++) {
+        		int dist = Math.abs(i-mid);
+        		int left = dist;
+        		int right = N - dist - 1;
+        		
+        		for(int j = left; j <= right; j++) answer += arr[i][j];
+        	}
+        	sb.append("#").append(test_case).append(" ").append(answer).append("\n");
+        }
+        System.out.print(sb);
+    }
 }
