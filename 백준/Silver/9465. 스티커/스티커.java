313505683d5920c69774335cea2097af66dada31
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2025-12-01 22:40:15 +0900

    [Silver I] Title: 스티커, Time: 696 ms, Memory: 140412 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int tc = 0; tc < T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+
+            int[][] scores = new int[N][2];
+
+            for (int i = 0; i < 2; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < N; j++) {
+                    scores[j][i] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int[][] dp = new int[N][3];
+
+            dp[0][0] = 0;
+            dp[0][1] = scores[0][0];
+            dp[0][2] = scores[0][1];
+
+            for (int i = 1; i < N; i++) {
+                dp[i][0] = Math.max(Math.max(dp[i - 1][0], dp[i - 1][1]), dp[i - 1][2]);
+                dp[i][1] = Math.max(dp[i - 1][0], dp[i - 1][2]) + scores[i][0];
+                dp[i][2] = Math.max(dp[i - 1][0], dp[i - 1][1]) + scores[i][1];
+            }
+
+            int answer = Math.max(Math.max(dp[N-1][0], dp[N-1][1]), dp[N-1][2]);
+
+            System.out.println(answer);
+        }
+    }
+}
