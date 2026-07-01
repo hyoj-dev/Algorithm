1755027b3dd75cb74a6629071d80cefacb545fcf
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-07-02 02:26:00 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 3.72 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129#) 
 
 ### 성능 요약
 
-메모리: 82.3 MB, 시간: 10.65 ms
+메모리: 81 MB, 시간: 3.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 02일 02:19:55
+2026년 07월 02일 02:25:57
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,27 +2,21 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        int cnt = 0, rmZero = 0;
-        
-        HashMap<Character, Integer> binaryMap = new HashMap<>();
+        int cnt = 0, one = 0, rmZero = 0;
         
         String binaryString = s;
         
         while(!binaryString.equals("1")) {
             for (char c : binaryString.toCharArray()) {
-                binaryMap.put(c, binaryMap.getOrDefault(c, 0) + 1);
+                if(c == '1') one++;
+                else rmZero++;
             }
-        
-            cnt++; 
-            rmZero += binaryMap.getOrDefault('0', 0);
-        
-            binaryString = Integer.toBinaryString(binaryMap.get('1'));
-            binaryMap.clear();
             
+            binaryString = Integer.toBinaryString(one);
+            cnt++;
+            one = 0;
         }
         
-        int[] answer = {cnt, rmZero};
-        
-        return answer;
+        return new int[] {cnt, rmZero};
     }
 }
