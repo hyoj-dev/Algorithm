05a99ba232c5f45288c1639a5f217119dedee0ab
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-07-01 18:33:32 +0900

    [level 2] Title: 숫자의 표현, Time: 0.42 ms, Memory: 57.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int left = 1, right = 1, sum = 1;
+        
+        while (left <= n) {
+            if (sum == n) {
+                answer++;
+                
+                sum -= left;
+                left++;
+            } else if (sum < n) {
+                right++;
+                sum += right;
+            } else {
+                sum -= left;
+                left++;
+            }
+        }
+        
+        return answer;
+    }
+}
