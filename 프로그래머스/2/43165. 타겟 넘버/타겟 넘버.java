3911b847e4c92df0cbb17a136e93df690ea22a18
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-07-23 23:55:17 +0900

    [level 2] Title: 타겟 넘버, Time: 5.90 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,28 @@
 import java.util.*;
 
 class Solution {
-    int[] arr;
-    int answer;
+    int[] numbers;
     int target;
+    int answer = 0;
     
-    public void dfs(int index, int sum) {
-        //1. 종료 조건
-        if(index == arr.length){
-            if(sum == target) {
-                answer++;    
+    public int solution(int[] numbers, int target) {
+        this.numbers = numbers;
+        this.target = target;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+    
+    private void dfs(int index, int sum) {
+        if(index == numbers.length) {
+            if(target == sum) {
+                answer++;
             }
             return;
         }
         
-        //2. 동작
-        dfs(index + 1, sum + arr[index]);
-        dfs(index + 1, sum - arr[index]);
-    }
-    
-    
-    public int solution(int[] numbers, int target) {
-        this.arr = numbers;
-        this.target = target;
-        
-        dfs(0,0);
-        
-        return answer;
+        dfs(index + 1, sum + numbers[index]);
+        dfs(index + 1, sum - numbers[index]);
     }
 }
