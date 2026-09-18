f53af567c1f0d2363712a90f4e8d5ae4bb4e6eeb
MyoJun <168921419+hyoj-dev@users.noreply.github.com>
2026-09-18 18:11:31 +0900

    [level 2] Title: H-Index, Time: 0.05 ms, Memory: 4.76 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end());
+    
+    int n = citations.size();
+    
+    for(int i = 0; i < n; i++) {
+        int h = n - i;
+        
+        if(citations[i] >= h) {
+            return h;
+        }
+    }
+    
+    return 0;
+}
